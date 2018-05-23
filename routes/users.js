5aaa9a19e6cee53003eab2d32f4f78388de53589
KELLY C FLYNN <kellyflynn3@msn.com>
2018-05-22 23:35:54 -0700

    update put

--- routes/users.js
+++ routes/users.js
@@ -37,7 +37,7 @@ router.get('/userlist', function(req, res) {
 //  users.update({name: 'foo'}, {name: 'bar'})
 
 /* Modify a user */
-router.put('/updateuser/:id', function(req, res) {
+/*router.put('/updateuser/:id', function(req, res) {
   var db = req.db;
   var collection = db.get('playerlist');
   var userToUpdate = req.params.id;
@@ -47,8 +47,22 @@ router.put('/updateuser/:id', function(req, res) {
       (err === null) ? { msg: '' } : { msg: err }
     );
     });
+});*/
+/* Modify a user */
+router.put('/updateuser/:id', function(req, res) {
+  var db = req.db;
+  var collection = db.get('playerlist');
+  var userToUpdate = req.params.id;
+    var bothParams = userToUpdate.split('*');
+    var ourMongoBalance = bothParams[1];
+    //we need a variable here that stores the user's balance
+  collection.update( { '_id': userToUpdate, }, 
+  { $set: { "MongoBalance": ourMongoBalance }}, function (err, doc, next) {  // can do more than one at a time
+      res.send(
+      (err === null) ? { msg: '' } : { msg: err }
+    );
+    });
 });
-
 
 
 module.exports = router;
